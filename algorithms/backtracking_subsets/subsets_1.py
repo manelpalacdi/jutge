d2b20e10aafdeb0819f2105a04cf2efa27acc705
manelpalacdi <manelpalacdi@gmail.com>
2024-06-25 12:28:35 +0200

    edit

--- algorithms/backtracking_subsets/subsets_1.py
+++ algorithms/backtracking_subsets/subsets_1.py
@@ -1,8 +1,6 @@
 # Write a program that, given n different words s1, …, sn, 
 # prints all the subsets that can be made up with the words.
 
-from itertools import chain, combinations
-
 def powerset(n, s):
     for i in range(1 << n):
         print(list(s[j] for j in range(n) if (i & (1 << j))))
